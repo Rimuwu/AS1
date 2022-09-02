0fb06126ed119c3b52c2c55d2ea76e4325316d69
AS1 <arseniykofanov@gmail.com>
2022-09-02 19:53:00 +0300

    Делаю сайт красивым. Главная страница доделана.

--- main/views.py
+++ main/views.py
@@ -14,9 +14,12 @@ def index(request):
 
     return render( request, 'main/index.html',
             { 'title': 'Главная страница сайта',
-              'notes': info_notes
+              'notes': info_notes,
             }
                  )
 
 def about_us(request):
     return render( request, 'main/about.html' )
+
+def page_not_found_view(request, exception):
+    return render(request, '404.html', status=404)
